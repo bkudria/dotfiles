fd2d0643c9937f75c6fd4b6a8af5eeb51e3dc465
Benjamin Kudria <ben@kudria.net>
2008-10-07 18:34:58 -0400

    smooth scrolling functions

--- .emacs.d/init-functions.el
+++ .emacs.d/init-functions.el
@@ -81,3 +81,14 @@ it)"
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
 
+; Smooth scrolling
+(defun smooth-scroll (increment)
+  (scroll-up increment) (sit-for 0.05)
+  (scroll-up increment) (sit-for 0.02)
+  (scroll-up increment) (sit-for 0.02)
+  (scroll-up increment) (sit-for 0.05)
+  (scroll-up increment) (sit-for 0.06)
+  (scroll-up increment))
+
+(global-set-key [(mouse-5)] '(lambda () (interactive) (smooth-scroll 1)))
+(global-set-key [(mouse-4)] '(lambda () (interactive) (smooth-scroll -1)))
