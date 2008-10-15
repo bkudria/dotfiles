49be37c186b424b6ce2882ad331b15bbdf43b717
Benjamin Kudria <ben@kudria.net>
2008-10-15 00:39:19 -0400

    tweak auto-frame sizes

--- .emacs.d/init-x-windows.el
+++ .emacs.d/init-x-windows.el
@@ -14,10 +14,10 @@
 (add-hook 'after-make-frame-functions 'fit-frame)
 (add-hook 'temp-buffer-show-hook 'fit-frame-if-one-window 'append)
 
-(setq-default fit-frame-empty-height 40) ; Set default frame resize options
-(setq-default fit-frame-empty-width 100)
-(setq-default fit-frame-min-height 40)
-(setq-default fit-frame-min-width 60)
+(setq-default fit-frame-empty-height 60) ; Set default frame resize options
+(setq-default fit-frame-empty-width 90)
+(setq-default fit-frame-min-height 60)
+(setq-default fit-frame-min-width 90)
 (setq-default fit-frame-max-height nil)
 (setq-default fit-frame-max-width nil)
 (setq-default fit-frame-max-height 95)
