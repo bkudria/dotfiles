de33457277b1229ad616819d599f71992d4845d2
Benjamin Kudria <ben@kudria.net>
2009-05-27 22:40:13 -0400

    remove broken stuff

--- .emacs.d/init-functions.el
+++ .emacs.d/init-functions.el
@@ -130,44 +130,11 @@
 
 (defalias 'qrr 'query-replace-regexp)
 
-(defun ido-goto-symbol ()
-  "Will update the imenu index and then use ido to select a symbol to navigate to"
-  (interactive)
-  (imenu--make-index-alist)
-  (let ((name-and-pos '())
-		(symbol-names '()))
-	(flet ((addsymbols (symbol-list)
-					   (when (listp symbol-list)
-						 (dolist (symbol symbol-list)
-						   (let ((name nil) (position nil))
-							 (cond
-							  ((and (listp symbol) (imenu--subalist-p symbol))
-							   (addsymbols symbol))
-
-							  ((listp symbol)
-							   (setq name (car symbol))
-							   (setq position (cdr symbol)))
-
-							  ((stringp symbol)
-							   (setq name symbol)
-							   (setq position (get-text-property 1 'org-imenu-marker symbol))))
-
-							 (unless (or (null position) (null name))
-							   (add-to-list 'symbol-names name)
-							   (add-to-list 'name-and-pos (cons name position))))))))
-	  (addsymbols imenu--index-alist))
-	(let* ((selected-symbol (ido-completing-read "Symbol? " symbol-names))
-		   (position (cdr (assoc selected-symbol name-and-pos))))
-	  (goto-char position))))
-
-
 (defun clone-current-buffer-in-new-frame ()
   "Creates a new frame viewing the current buffer, and switches the parent frame to *scratch*"
   (interactive)
   (make-frame)
   (switch-to-buffer (get-buffer "*scratch*")))
-
-
 
 (defun nuke-all-buffers ()
   "Kill all buffers, leaving *scratch* only."
@@ -176,20 +143,6 @@
 	  (buffer (buffer-list))
 	(kill-buffer buffer))
   (delete-other-windows))
-
-
-
-(defun ido-find-tag ()
-  "Find a tag using ido"
-  (interactive)
-  (tags-completion-table)
-  (let (tag-names)
-	(mapc (lambda (x)
-			(unless (integerp x)
-			  (push (prin1-to-string x t) tag-names)))
-		  tags-completion-table)
-	(find-tag (ido-completing-read "Tag: " tag-names))))
-
 
 ;; Use ido to find files in tags file
 (defun ido-find-file-in-tag-files ()
