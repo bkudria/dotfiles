f387d69a5a8015218efd19ebdd81d2bf3e9f7ce4
Benjamin Kudria <ben@kudria.net>
2009-11-05 17:44:19 -0800

    set a random seed and shuffle prompt colors so similar hostnames get different colors

--- bin/hosts.rb
+++ bin/hosts.rb
@@ -8,6 +8,8 @@ class Array
 	end
 end
 
+srand "benkudria".split('').inject(1) {|a, c| a*c[0]}
+
 @options = {:color => :dark}
 
 @hostnames =
@@ -71,17 +73,17 @@ end
 
 @lightlist = [66..66, 131..131, 143..146].flatten_ranges
 @light_colors = ([20..21, 25..51, 56..57,
-                  61..87, 90..231].flatten_ranges - @lightlist).reverse
+                  61..87, 90..231].flatten_ranges - @lightlist).reverse.shuffle
 
 @dark_colors = [16..33, 52..71, 88..100,
-                124..137, 160..178, 196..208].flatten_ranges
+                124..137, 160..178, 196..208].flatten_ranges.shuffle
 
 @scary_light_colors = [124..125, 130..130,
                        160..161, 166..167, 172..172, 196..197,
-                       202..203, 208..210, 220..221].flatten_ranges
+                       202..203, 208..210, 220..221].flatten_ranges.shuffle
 
 @scary_dark_colors = [124..125, 160..162,
-                      166..168, 196..198, 202..204, 208..209].flatten_ranges
+                      166..168, 196..198, 202..204, 208..209].flatten_ranges.shuffle
 
 def hash_chars(string)
 	string.split('').inject{|sum, char| (sum.to_i ^ char[0]) % 131 }
