885063d00af038e6d2cc74769ce0eea1c36fd9ba
Benjamin Kudria <ben@kudria.net>
2016-11-26 15:38:39 -0800

    Remove old emacs

--- .emacs.d.old/init-elpa.el
+++
@@ -1,8 +0,0 @@
-
-(setq-default package-archives '(("ELPA" . "http://tromey.com/elpa/") 
-                           ("gnu" . "http://elpa.gnu.org/packages/")))
-
-(when
-    (load
-     (expand-file-name "~/.emacs.d/elpa/package.el"))
-  (package-initialize))
--- .emacs.d.old/init-projects.el
+++
@@ -1,6 +0,0 @@
-;; Initialize a few variables
-(setq-default project-base-dir nil)
-
-(dir-locals "~/projects/work/younoodle/younoodle.org"
-            `((nil . ((project-base-dir . ,(concat base-dir "/"))
-                      (tags-file-name   . ,(concat base-dir "/.etags"))))))
