bfa07f1e4fedc94d167f8ee0c7301e5c1efea0db
Ben Kudria <ben.kudria@cloverhealth.com>
2016-12-05 10:48:45 -0800

    Update emacs config, use as editor

--- .spacemacs.d/layers/bkudria/packages.el
+++ .spacemacs.d/layers/bkudria/packages.el
@@ -3,12 +3,18 @@
   '(
     evil-extra-operator
     (evil-textobj-line :location (recipe :fetcher github :repo "syohex/evil-textobj-line"))
+    evil-replace-with-register
     ))
 
 (defun bkudria/init-evil-extra-operator ()
   (use-package evil-extra-operator :defer t
     :init
     (define-key evil-motion-state-map "gl" 'evil-operator-clone)))
+
+(defun bkudria/init-evil-replace-with-register ()
+  (use-package evil-extra-operator :defer t
+    :init
+    (define-key evil-motion-state-map "gr" 'evil-replace-with-register)))
 
 (defun bkudria/init-evil-textobj-line ()
   (use-package evil-textobj-line))
