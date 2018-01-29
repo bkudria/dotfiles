86f6a247f4590902e44f93bfaa806888b48858ff
Benjamin Kudria <ben@kudria.net>
2018-01-28 22:33:42 -0800

    Quiet gtags

--- .git_template/hooks/gtags.sh
+++ .git_template/hooks/gtags.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -f .gtags.ignore ]; then
-    git ls-files | grep -vf .gtags.ignore | gtags --gtagslabel=pygments -i -v -f-
+    git ls-files | grep -vf .gtags.ignore | gtags --gtagslabel=pygments -i -q -f-
 else
-    git ls-files | gtags --gtagslabel=pygments -i -v -f-
+    git ls-files | gtags --gtagslabel=pygments -i -q -f-
 fi
