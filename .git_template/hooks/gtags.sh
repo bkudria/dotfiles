09bc13b742349de148f3be229a19cec232465fd0
Benjamin Kudria <ben@kudria.net>
2019-02-28 12:54:42 -0800

    Minor updates

--- .git_template/hooks/gtags.sh
+++ .git_template/hooks/gtags.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ -f .gtags.ignore ]; then
-    git ls-files | grep -vf .gtags.ignore | gtags --gtagslabel=pygments -i -q -f-
+    git ls-files | grep -vf .gtags.ignore | /usr/local/bin/gtags --gtagslabel=pygments -i -q -f-
 else
-    git ls-files | gtags --gtagslabel=pygments -i -q -f-
+    git ls-files | /usr/local/bin/gtags --gtagslabel=pygments -i -q -f-
 fi
