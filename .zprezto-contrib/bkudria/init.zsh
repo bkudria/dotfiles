4eede367212ac3615d738b9713f51e28d33c855c
Benjamin Kudria <ben@kudria.net>
2019-04-08 11:24:06 -0700

    bat

--- .zprezto-contrib/bkudria/init.zsh
+++ .zprezto-contrib/bkudria/init.zsh
@@ -4,6 +4,7 @@ export VISUAL='emacsclient -q'
 alias ll='exa -lF --colour-scale --group-directories-first'
 alias la='ll -a'
 alias git=hub
+alias cat=bat
 
 path=("$HOME/bin" $path)
 cdpath=($HOME/Code)
