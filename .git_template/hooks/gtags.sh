8d22332fad7fc6ccfafabca1dfbb8db1163333d1
Benjamin Kudria <ben@kudria.net>
2020-02-21 12:28:45 -0800

    nope

--- .git_template/hooks/gtags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-if [ -f .gtags.ignore ]; then
-    git ls-files | grep -vf .gtags.ignore | /usr/local/bin/gtags --gtagslabel=pygments -i -q -f-
-else
-    git ls-files | /usr/local/bin/gtags --gtagslabel=pygments -i -q -f-
-fi
