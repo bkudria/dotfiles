4579c963d859bb0a0d71dc2ad939cfc32d2451e4
Benjamin Kudria <ben@kudria.net>
2023-07-27 15:46:18 -0700

    doom upgrade, mermaid

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -16,6 +16,8 @@
 (package! mini-frame)
 (package! jsonnet-mode)
 (package! protobuf-mode)
+(package! mermaid-mode)
+;; (package! topspace)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
